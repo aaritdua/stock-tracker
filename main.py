96896092d9800de662e8d37ce8b8d2b6f7faf7a2
aaritdua <aaritduaofficial@gmail.com>
2026-02-24 10:58:10 -0500

    sentiment score added to features and outputted also

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.fetcher import get_stock_data
 from src.features import build_features
 from src.model import training_model, evaluate_model, predict_next_close
+from src.sentiment import get_sentiment_score, create_sentiment_df
 
 SYMBOLS = [
         "GOOG",   # Google - Tech
@@ -31,11 +32,14 @@ END_DATE = "2026-02-20"
 def main():
     for SYMBOL in SYMBOLS:
         stock_data = get_stock_data(SYMBOL, START_DATE, END_DATE)
-        stock_data = build_features(stock_data)
+        sentiment_df = create_sentiment_df(SYMBOL, START_DATE, END_DATE)
+        stock_data = build_features(stock_data, sentiment_df)
         model, X_test, y_test = training_model(stock_data)
         mae, rmse, r2 = evaluate_model(model, X_test, y_test)
         prediction = predict_next_close(model, stock_data)
         print(f"Predicted next close for {SYMBOL}: ${prediction:.2f}")
+        sentiment = get_sentiment_score(SYMBOL)
+        print(f"Sentiment score for {SYMBOL}: {sentiment:.2f}")
 
 
 if __name__ == "__main__":
